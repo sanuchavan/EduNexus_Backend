3fe2f07e3bd6127b3854c6c65fe46fe696a64148
sanuchavan <sanikachavan2034@gmail.com>
2025-11-03 09:05:27 +0530

    Uploaded complete Java Spring Boot backend code

---
+++ StudentLearningApp/src/test/java/com/tka/StudentLearningApp/StudentLearningAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tka.StudentLearningApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentLearningAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
